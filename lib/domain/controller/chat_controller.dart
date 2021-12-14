f62d28d74c9442db0ec64596dfc89bfde082eb4f
rportos <roberto.porto.solano@gmail.com>
2021-12-14 12:29:52 -0500

    cambios en el controlador

--- lib/domain/controller/chat_controller.dart
+++ lib/domain/controller/chat_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:f_202110_firebase/data/model/message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -7,25 +8,55 @@ import 'package:loggy/loggy.dart';
 class ChatController extends GetxController {
   final databaseReference = FirebaseDatabase.instance.reference();
   var messages = <Message>[].obs;
+  late StreamSubscription<Event> newEntryStreamSubscription;
+  late StreamSubscription<Event> updateEntryStreamSubscription;
 
-  //Método start para comenzar a escuchar la colección de documentos FlutterMessages,   
-  //que contiene el mensaje a enviar en los chats   
-  // start() {}
+  //Método start para comenzar a escuchar la colección de documentos FlutterMessages,
+  //que contiene el mensaje a enviar en los chats
 
-  //Método onEntryChanged listener  
-  //para añadir al listado los mensajes que llegan de la db
-  // _onEntryChanged(Event event) {}
+  start() {
+    messages.clear();
+    newEntryStreamSubscription = databaseReference
+        .child("fluttermessages")
+        .onChildAdded
+        .listen(_onEntryAdded);
 
-  //Método stop para dejar de escuchar la colección
-  // stop() {}
+    updateEntryStreamSubscription = databaseReference
+        .child("fluttermessages")
+        .onChildChanged
+        .listen(_onEntryChanged);
+  }
+
+  stop() {
+    newEntryStreamSubscription.cancel();
+    updateEntryStreamSubscription.cancel();
+  }
+
+  _onEntryChanged(Event event) {
+    print("Something was changed");
+    var oldEntry = messages.singleWhere((entry) {
+      return entry.key == event.snapshot.key;
+    });
+    messages[messages.indexOf(oldEntry)] = Message.fromSnapshot(event.snapshot);
+  }
 
   _onEntryAdded(Event event) {
     print("Something was added");
     messages.add(Message.fromSnapshot(event.snapshot));
   }
 
-  //Método sendmsg para enviar un nuevo mensaje
-  // Future<void> sendMsg(String text) async {}
+  Future<void> sendMsg(String text) async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    try {
+      databaseReference
+          .child("fluttermessages")
+          .push()
+          .set({'text': text, 'uid': uid});
+    } catch (error) {
+      logError("Error sending msg $error");
+      return Future.error(error);
+    }
+  }
 
   Future<void> updateMsg(Message message) async {
     logInfo('updateMsg with key ${message.key}');
