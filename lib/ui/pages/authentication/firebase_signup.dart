bf0fb9f6a9988603bf70367613967b151f3ae310
rportos <roberto.porto.solano@gmail.com>
2021-12-14 12:30:10 -0500

    agregando la libreria

--- lib/ui/pages/authentication/firebase_signup.dart
+++ lib/ui/pages/authentication/firebase_signup.dart
@@ -1,5 +1,4 @@
 import 'package:f_202110_firebase/domain/controller/authentication_controller.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
